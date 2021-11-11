036a26e9f815bd65bcfd817aedcf4c745dcb4e40
Abubakar <abubakarhyaat@gmail.com>
2021-11-11 19:29:09 +0500

    Added exam activity

--- app/src/main/java/com/abubakar/makhrijal_huruf/ExamActivity.java
+++ app/src/main/java/com/abubakar/makhrijal_huruf/ExamActivity.java
@@ -3,12 +3,19 @@ package com.abubakar.makhrijal_huruf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class ExamActivity extends AppCompatActivity {
 
+    RadioGroup radioGroup;
+    RadioButton radiobtn;
+    TextView txtViewQs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam);
+
     }
 }
